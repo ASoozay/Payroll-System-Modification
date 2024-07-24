ce20951d50289c61df9ab5305682acdcd3a1f38f
Andrew Soozay <asousa2021@gmail.com>
2024-07-23 19:04:08 -0700

    Update HourlyEmployee.java

--- src/HourlyEmployee.java
+++ src/HourlyEmployee.java
@@ -1,7 +1,5 @@
-// Andrew Sousa
+// Andrew Soozay
 // 7/14/24
-// CS 145
-// Programming Assignment 4: Payment System Heirarchy
 // HourlyEmployee.java
 
 // HourlyEmployee.java is a class that contains and calculates all informaiton for an hourly employee
